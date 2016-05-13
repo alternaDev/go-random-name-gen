ee7414bdd1baf7c6f581e14f5cedef3fe1a48e4b
Jan-Henrik Bruhn <jh.bruhn97@gmail.com>
2016-05-13 10:59:08 +0200

    Added Benchmark

--- generator_test.go
+++ generator_test.go
@@ -14,6 +14,17 @@ func TestGenerator(t *testing.T) {
   fmt.Println(nameString)
 }
 
+func BenchmarkGenerator(b *testing.B) {
+  for n := 0; n < b.N; n++ {
+    nameString, err := GenerateName(1, 1, 3)
+    if err != nil {
+      b.Error(err)
+    }
+
+    fmt.Println(nameString)
+  }
+}
+
 func TestPossibilities(t *testing.T) {
   fmt.Println(GetPossibilities(1, 1, 3))
 }
